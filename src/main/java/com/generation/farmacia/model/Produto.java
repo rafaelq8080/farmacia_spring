4634907e4e4fee996ade7d4d4e269bd13cfcbe30
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-07-01 15:30:22 -0300

    :tada: Commit Inicial

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,85 @@
+package com.generation.farmacia.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@Entity
+@Table(name = "tb_produtos_farmacia")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message="Nome é obrigatório!")
+	private String nome;
+	
+	/**
+	 * A anotação @JsonFormat(shape = JsonFormat.Shape.STRING) é utilizada 
+	 * para formatar o valor do preço do produto como uma String. Desta forma,
+	 * conseguiremos visualizar a parte decimal do preço mesmo sendo 00.
+	 */
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	@NotNull(message="Preço é obrigatório!")
+	@Positive(message = "Digite um valor maior do que zero")
+	private BigDecimal preco;
+	
+	private String foto;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getFoto() {
+		return this.foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
